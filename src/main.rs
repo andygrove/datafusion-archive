ccac5606d03b811242137a2cbd73ba502077bd10
Andy Grove <andygrove73@gmail.com>
2016-12-31 11:09:54 -0700

    implement basic expression evaluation against a tuple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-//use std::error::Error;
+#![feature(box_patterns)]
+
+use std::error::Error;
 
 #[derive(Debug)]
 enum DataType {
@@ -18,15 +20,29 @@ struct TupleType {
     columns: Vec<ColumnType>
 }
 
+#[derive(Debug,Clone)]
 enum Value {
     UnsignedLong(u64),
-    String(String)
+    String(String),
+    Boolean(bool)
 }
 
 /// The tuple trait provides type-safe access to individual values within the tuple
 trait Tuple {
-    fn get_unsigned_long(index: u32) -> u64;
-    fn get_string(index: u32) -> String;
+    fn get_value(&self, index: usize) -> Result<Value, Box<Error>>;
+}
+
+#[derive(Debug)]
+struct SimpleTuple {
+    values: Vec<Value>
+}
+
+impl Tuple for SimpleTuple {
+
+    fn get_value(&self, index: usize) -> Result<Value, Box<Error>> {
+        Ok(self.values[index].clone())
+    }
+
 }
 
 enum Operator {
@@ -40,21 +56,46 @@ enum Operator {
 
 enum Expr {
     /// index into a value within the tuple
-    TupleValue(u32),
+    TupleValue(usize),
     /// literal value
     Literal(Value),
     /// binary expression e.g. "age > 21"
     BinaryExpr { left: Box<Expr>, op: Operator, right: Box<Expr> },
 }
 
-//enum PlanNode {
+enum PlanNode {
 //    TableScan,
 //    IndexScan,
-//    Filter,
+    Filter(Expr),
 //    Sort,
 //    Project,
 //    Join
-//}
+}
+
+fn evaluate(tuple: &Box<Tuple>, tt: &TupleType, expr: &Expr) -> Result<Value, Box<Error>> {
+
+    match expr {
+        &Expr::BinaryExpr { box ref left, ref op, box ref right } => {
+            let left_value = evaluate(tuple, tt, left).unwrap();
+            let right_value = evaluate(tuple, tt, right).unwrap();
+            match op {
+                &Operator::Eq => {
+                    match left_value {
+                        Value::UnsignedLong(l) => match right_value {
+                            Value::UnsignedLong(r) => Ok(Value::Boolean(l == r)),
+                            _ => Err(From::from("oops"))
+                        },
+                        _ => Err(From::from("oops"))
+                    }
+                },
+                _ => Err(From::from("oops"))
+            }
+        },
+        &Expr::TupleValue(index) => tuple.get_value(index),
+        &Expr::Literal(ref value) => Ok(value.clone()),
+    }
+
+}
 
 fn main() {
 
@@ -66,5 +107,26 @@ fn main() {
     };
 
     println!("Tuple type: {:?}", tt);
+
+    let data : Vec<Box<Tuple>> = vec![
+        Box::new(SimpleTuple { values: vec![ Value::UnsignedLong(1), Value::String(String::from("Alice")) ] }),
+        Box::new(SimpleTuple { values: vec![ Value::UnsignedLong(2), Value::String(String::from("Bob")) ] }),
+    ];
+
+    // create simple filter expression
+    let filterExpr = Expr::BinaryExpr {
+        left: Box::new(Expr::TupleValue(0)),
+        op: Operator::Eq,
+        right: Box::new(Expr::Literal(Value::UnsignedLong(2)))
+    };
+
+    //let plan = PlanNode::Filter(filterExpr);
+
+    // iterate over tuples and evaluate the plan
+    for tuple in &data {
+        let x = evaluate(tuple, &tt, &filterExpr).unwrap();
+
+        print!("filter expr evaluates to {:?}", x);
+    }
 
 }
