6983dfbbdf8c45d5aa64b0d6d992bf3d48783070
Andy Grove <andygrove73@gmail.com>
2017-02-12 11:44:01 -0700

    can tokenize SELECT 1

--- src/parser.rs
+++ src/parser.rs
@@ -11,11 +11,12 @@ use super::sql::*;
 //    Where
 //}
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,PartialEq)]
 pub enum Token {
     Identifier(String),
     Keyword(String),
     Operator(String),
+    Number(String),
     Whitespace
 }
 
@@ -40,21 +41,55 @@ impl Tokenizer {
             tokens.push(token);
         }
 
-        Ok(tokens)
+        Ok(tokens.into_iter().filter(|t| match t {
+            &Token::Whitespace => false,
+            _ => true
+        }).collect())
     }
 
     fn next_token(&self, chars: &mut Peekable<Chars>) -> Result<Option<Token>, ParserError> {
         match chars.peek() {
             Some(&ch) => match ch {
+                // whitespace
                 ' ' | '\t' | '\n' => {
-                    chars.next();
+                    chars.next(); // consume
                     Ok(Some(Token::Whitespace))
                 },
-                _ => Err(ParserError::TokenizerError(String::from("unhandled case in tokenizer")))
+                // identifier or keyword
+                'a' ... 'z' | 'A' ... 'Z' | '_' | '@' => {
+                    let mut s = String::new();
+                    while let Some(&ch) = chars.peek() {
+                        match ch {
+                            'a' ... 'z' | 'A' ... 'Z' | '_' | '0' ... '9' => {
+                                chars.next(); // consume
+                                s.push(ch);
+                            },
+                            _ => break
+                        }
+                    }
+                    Ok(Some(Token::Identifier(s)))
+                },
+                // numbers
+                '0' ... '9' => {
+                    let mut s = String::new();
+                    while let Some(&ch) = chars.peek() {
+                        match ch {
+                            '0' ... '9' => {
+                                chars.next(); // consume
+                                s.push(ch);
+                            },
+                            _ => break
+                        }
+                    }
+                    Ok(Some(Token::Number(s)))
+                },
+                _ => Err(ParserError::TokenizerError(
+                    String::from(format!("unhandled char '{}' in tokenizer", ch))))
             },
             None => Ok(None)
         }
     }
+
 
 }
 
@@ -70,6 +105,8 @@ mod tests {
         let tokens = tokenizer.tokenize().unwrap();
         println!("tokens = {:?}", tokens);
         assert_eq!(2, tokens.len());
+        assert_eq!(Token::Identifier(String::from("SELECT")), tokens[0]);
+        assert_eq!(Token::Number(String::from("1")), tokens[1]);
     }
 }
 
