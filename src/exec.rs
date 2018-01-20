cb9ea4b7e466f44afeca63c420f096e1e918bd32
Andy Grove <andygrove73@gmail.com>
2018-01-20 14:14:39 -0700

    Save

--- src/exec.rs
+++ src/exec.rs
@@ -7,16 +7,41 @@ struct QueryExecutor {}
 
 impl QueryExecutor {
 
-    fn execute(plan: &Rel) -> Result<Box<Relation>,String> {
+    fn execute(&self, plan: &Rel) -> Result<Box<Relation>,String> {
         match plan {
             &Rel::CsvFile { ref filename, ref schema } =>
                 Ok(Box::new(CsvRelation::open(filename.to_string(), schema.clone()))),
+
+            &Rel::Selection { ref expr, box ref input } => {
+//                let input_rel = self.execute(&input)?;
+//                Ok(Box::new(FilterRelation {
+//                    input: input_rel,
+//                    schema: input_rel.schema().clone()
+//                }))
+                unimplemented!("selection")
+            },
+
             _ => Err("not implemented".to_string())
         }
     }
 
 }
 
+
+struct FilterRelation<'a> {
+    schema: TupleType,
+    input: Box<Relation<'a>>
+}
+
+impl<'a> Relation<'a> for FilterRelation<'a> {
+    fn schema(&'a self) -> TupleType {
+        unimplemented!()
+    }
+
+    fn scan(&'a mut self) -> Box<Iterator<Item=Tuple> + 'a> {
+        unimplemented!()
+    }
+}
 
 /// Evaluate a relational expression against a tuple
 pub fn evaluate(tuple: &Tuple, tt: &TupleType, rex: &Rex) -> Result<Value, Box<Error>> {
