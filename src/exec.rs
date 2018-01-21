97a569165d37e4048c3fa5d3e474929da1a90503
Andy Grove <andygrove73@gmail.com>
2018-01-21 12:57:37 -0700

    implement scan  for in-memory relation

--- src/exec.rs
+++ src/exec.rs
@@ -44,14 +44,15 @@ impl<'a> CsvRelation<'a> {
     }
 
 
-    /// Covnert StringRecord into our internal tuple type based on the known schema
+    /// Convert StringRecord into our internal tuple type based on the known schema
     fn create_tuple(r: &StringRecord, schema: &TupleType) -> Result<Tuple,ExecutionError> {
         assert_eq!(schema.columns.len(), r.len());
         let values = schema.columns.iter().zip(r.into_iter()).map(|(c,s)| match c.data_type {
+            //TODO: remove unwrap use here
             DataType::UnsignedLong => Value::UnsignedLong(s.parse::<u64>().unwrap()),
             DataType::String => Value::String(s.to_string()),
             DataType::Double => Value::Double(s.parse::<f64>().unwrap()),
-        }).collect::<Vec<Value>>();
+        }).collect();
         Ok(Tuple::new(values))
     }
 }
@@ -59,43 +60,37 @@ impl<'a> CsvRelation<'a> {
 pub trait SimpleRelation<'a> {
 
     /// scan all records in this relation
-    fn scan(&'a self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a>;
-
-    /// scan all records in this relation that match the given predicate
-    fn scan_with_predicate(&'a self, predicate: Rex) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a>;
+    fn scan(&self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a>;
 }
 
 impl<'a> SimpleRelation<'a> for CsvRelation<'a> {
 
-    fn scan(&'a self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a> {
+    fn scan(&self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a> {
+
+        let CsvRelation { file, schema } = *self;
 
         let buf_reader = BufReader::new(self.file);
         let csv_reader = csv::Reader::from_reader(buf_reader);
         let record_iter = csv_reader.into_records();
 
-        let tuple_iter = record_iter.map(move |r| match r {
-            Ok(record) => CsvRelation::create_tuple(&record, self.schema),
+        let tuple_iter = record_iter.map(move|r| match r {
+            Ok(record) => CsvRelation::create_tuple(&record, &schema),
             Err(_) => Err(ExecutionError::Custom("TODO".to_string()))
         });
 
         Box::new(tuple_iter)
     }
 
-    fn scan_with_predicate(&'a self, predicate: Rex) -> Box<Iterator<Item=Result<Tuple, ExecutionError>> + 'a> {
-        unimplemented!()
-    }
 }
 
-//impl<'a> SimpleRelation<'a> for InMemoryRelation<'a> {
-//
-//    fn scan(&'a mut self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a> {
-//        // transform trom Tuple to Result<Tuple,_>
-////        let tuple_results = self.tuples.iter().map(move |t| Ok(t.clone()));
-////        Box::new(tuple_results)
-//        unimplemented!()
-//    }
-//
-//}
+impl<'a> SimpleRelation<'a> for InMemoryRelation<'a> {
+
+    fn scan(&self) -> Box<Iterator<Item=Result<Tuple,ExecutionError>> + 'a> {
+        let tuple_iter = self.tuples.iter().map(move |t| Ok(t.clone()));
+        Box::new(tuple_iter)
+    }
+
+}
 
 
 
