3778258b1f2b0df2dbbb035d855e38c4a8d9f5cc
Andy Grove <andygrove73@gmail.com>
2018-02-10 17:09:24 -0700

    add dockerfile

--- bin/create-docker-image.sh
+++ bin/create-docker-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#cargo clean
-#cargo build --target=x86_64-unknown-linux-musl --release
+cargo clean
+cargo build --target=x86_64-unknown-linux-musl --release
 docker build -t datafusionrs/datafusion .
 
