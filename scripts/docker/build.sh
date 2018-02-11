b62807a1f08a99fdd3e6d1bc24292cbbe853ed33
Kenji Suenobu <kenji@kenji.attlocal.net>
2018-02-11 13:45:27 -0600

    Corrected build variables with a dash - forgot about that...

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -5,19 +5,19 @@
 set -e 
 
 # DOCKER_REPOSITORY is the master repository where the docker image will be referenced and stored.  Defaults to "datafusionrs"
-DOCKER_REPOSITORY="${DOCKER_REPOSITORY:datafusionrs}"
+DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-datafusionrs}"
 
 # DOCKER_VERSION is the current version of the docker image.  Defaults to "0.1.4"
-DOCKER_VERSION="${DOCKER_VERSION:0.1.4}"
+DOCKER_VERSION="${DOCKER_VERSION:-0.1.4}"
 
 # DOCKER_TAG is the combined tag from the docker repository and version.  Defaults to (repository)/datafusion:(version)
-DOCKER_TAG="${DOCKER_REPOSITORY}/datafusion:${DOCKER_VERSION}"
+DOCKER_TAG="${DOCKER_TAG:-${DOCKER_REPOSITORY}/datafusion:${DOCKER_VERSION}}"
 
 # Note: Setting DOCKER_PUSH to "true" outside of this script will cause the docker container to push to a repository.
-DOCKER_PUSH="${DOCKER_PUSH:false}"
+DOCKER_PUSH="${DOCKER_PUSH:-false}"
 
 # BUILD_RELEASE set to true will perform a "cargo build --release"; otherwise this step will be skipped.  Defaults to "false"
-BUILD_RELEASE="${BUILD_RELEASE:false}"
+BUILD_RELEASE="${BUILD_RELEASE:-false}"
 
 # Build the final release candidate, create a docker container from it.  Only builds for x86_6_64-unknown-linux-musl target.
 if [ "${TARGET}" == "x86_64-unknown-linux-musl" ]; then
