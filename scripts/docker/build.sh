2c2a1287196d69f52b9d7e57d2db97c672993b85
Kenji Suenobu <ksuenobu@fastmail.com>
2018-02-17 16:09:26 -0600

    Updated to call release and not debug build.

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -13,7 +13,7 @@ DOCKER_PUSH="${DOCKER_PUSH:-false}"
 # Build the final release candidate, create a docker container from it.  Only builds for x86_6_64-unknown-linux-musl target.
 if [ -f "target/release/console" ]; then 
   # DATAFUSION_VERSION is the current version of the docker image.
-  DATAFUSION_VERSION=`./target/debug/console --version | awk -F' ' '{ print $3 }'`
+  DATAFUSION_VERSION=`target/release/console --version | awk -F' ' '{ print $3 }'`
 
   # DOCKER_TAG is the combined tag from the docker repository and version.  Defaults to (repository)/datafusion:(version)
   DOCKER_TAG="${DOCKER_TAG:-${DOCKER_REPOSITORY}/datafusion:${DATAFUSION_VERSION}-SNAPSHOT}"
