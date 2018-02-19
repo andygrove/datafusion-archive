a726d069a2a39fe1f6c071a33a97ce1cfd6d016f
Andy Grove <andygrove73@gmail.com>
2018-02-19 10:59:31 -0700

    docker packaging

--- scripts/docker/entrypoint.sh
+++ scripts/docker/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo Starting DataFusion worker node ...
-/opt/datafusion/bin/worker --etcd 127.0.0.1 --bind 0.0.0.0 --register=127.0.0.1 --webroot /opt/datafusion/www --data_dir=/tmp
+#TODO --bind 0.0.0.0 --register 127.0.0.1
+/opt/datafusion/bin/worker --etcd http://127.0.0.1:2379 --webroot /opt/datafusion/www --data_dir /tmp
