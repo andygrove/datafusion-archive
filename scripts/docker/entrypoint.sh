162cdb00fe79518641acb6104cb063ce06575329
Andy Grove <andygrove73@gmail.com>
2018-02-19 17:50:47 -0700

    save work on docker packaging

--- scripts/docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo Starting DataFusion worker node ...
-#TODO --bind 0.0.0.0 --register 127.0.0.1
-/opt/datafusion/bin/worker --etcd http://127.0.0.1:2379 --webroot /opt/datafusion/www --data_dir /tmp
--- scripts/docker/worker/build.sh
+++ scripts/docker/worker/build.sh
@@ -7,16 +7,13 @@ set -e
 # get the product version from Cargo.toml
 DATAFUSION_VERSION=`grep version Cargo.toml | head -1 | awk -F' ' '{ print $3 }' | sed 's/\"//g'`
 
-# DOCKER_REPOSITORY is the master repository where the docker image will be referenced and stored.  Defaults to "datafusionrs"
-DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-datafusionrs}"
-
 # Note: Setting DOCKER_PUSH to "true" outside of this script will cause the docker container to push to a repository.
 DOCKER_PUSH="${DOCKER_PUSH:-false}"
 
 # DOCKER_TAG is the combined tag from the docker repository and version.  Defaults to (repository)/datafusion:(version)
-DOCKER_TAG="${DOCKER_TAG:-${DOCKER_REPOSITORY}/datafusion:${DATAFUSION_VERSION}}"
+DOCKER_TAG="${DOCKER_TAG:-datafusionrs/worker:${DATAFUSION_VERSION}}"
 
 echo "Building docker: ${DOCKER_TAG}"
-docker build -f scripts/docker/Dockerfile -t "${DOCKER_TAG}" .
+docker build -f scripts/docker/worker/Dockerfile -t "${DOCKER_TAG}" .
 
 #docker push "${DOCKER_TAG}"
