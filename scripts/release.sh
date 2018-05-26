775f97570f77a674c1dae95738625017bce881d6
Andy Grove <andygrove73@gmail.com>
2018-05-26 09:10:33 -0600

    Release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,17 +6,29 @@ DATAFUSION_VERSION=`grep version Cargo.toml | head -1 | awk -F' ' '{ print $3 }'
 
 echo Version: $DATAFUSION_VERSION
 
+# Make sure there are no uncommitted changes
+git diff-index --quiet HEAD --
+
+# Run tests
 cargo test
 
+# Run examples
 cargo run --example csv_sql
 cargo run --example csv_dataframe
+cargo run --example ndjson_sql
 cargo run --example parquet_sql
 cargo run --example parquet_dataframe
 
+# Build Docker image
 ./scripts/docker/console/build.sh
 
+# Publish crate
 cargo publish
+
+# Push Docker image
 docker push datafusionrs/console:$DATAFUSION_VERSION
 docker push datafusionrs/console:latest
+
+# Tag release
 git tag $DATAFUSION_VERSION
 git push origin :$DATAFUSION_VERSION
